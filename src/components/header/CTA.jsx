373dc709033c2437573128856e22fe1f16b01084
Kumar Chaudhary <102680201+Coomar25@users.noreply.github.com>
2024-04-20 10:16:11 +0545

    CV_Added

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import CV from '../../assets/KumarChaudhary-CV.pdf'
+import CV from '../../assets/Kumar_Chaudhary_March_2024.pdf'
 import { Link } from 'react-router-dom'
 
 
 const CTA = () => {
     return (
         <div className="cta">
-            <a href={CV} download className='btn'>Donwnload CV</a>
+            <a href={CV} download className='btn'>Download CV</a>
             <Link to='/contact'> <a href='#talk' className='btn btn-primary' >Let's Talk </a></Link>
 
         </div>
